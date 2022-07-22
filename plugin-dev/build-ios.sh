9edad25562296a641e568cda5cad6f3f5609b985
PatriceJiang <397136899@qq.com>
2022-07-22 16:07:24 +0800

    save

--- plugin-dev/build-ios.sh
+++ plugin-dev/build-ios.sh
@@ -2,11 +2,12 @@
 set -x
 set -e
 
-rm -rf build-ios
+#rm -rf build-ios
 rm -rf dist/ios
 
 
-cmake -GNinja \
+
+COCOS_ENGINE_DEV=0 cmake -GXcode \
     -B build-ios/arm64 \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=11 \
     -DCMAKE_INSTALL_PREFIX=build-ios/install/arm64 \
@@ -14,9 +15,10 @@ cmake -GNinja \
     -DCMAKE_OSX_SYSROOT=iphoneos \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_OSX_ARCHITECTURES=arm64
-ninja -C build-ios/arm64 install
+#ninja -C build-ios/arm64 install
+cmake --build build-ios/arm64 --target install --config=Release
 
-cmake -GNinja \
+COCOS_ENGINE_DEV=0 cmake -GXcode \
     -B build-ios/x86_64 \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=11 \
     -DCMAKE_INSTALL_PREFIX=build-ios/install/x86_64 \
@@ -24,13 +26,14 @@ cmake -GNinja \
     -DCMAKE_OSX_SYSROOT=iphonesimulator \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_OSX_ARCHITECTURES=x86_64
-ninja -C build-ios/x86_64 install
+#ninja -C build-ios/x86_64 install
+cmake --build build-ios/x86_64 --target install --config=Release
 
 mkdir -p dist/ios/include dist/ios/lib
-cp -rv build-ios/install/arm64/include/* dist/ios/include/ 
+#cp -rv build-ios/install/arm64/include/* dist/ios/include/ 
 xcrun lipo -create \
-    build-ios/install/arm64/lib/libAES.a \
-    build-ios/install/x86_64/lib/libAES.a \
+    build-ios/install/arm64/lib/libhello_cocos.a \
+    build-ios/install/x86_64/lib/libhello_cocos.a \
     -output \
-    dist/ios/lib/libAES.a
-xcrun strip -S `find build-ios -name "*AES.a"`
+    dist/ios/lib/libhello_cocos.a
+xcrun strip -S `find build-ios -name "*hello_cocos.a"`
--- plugin-dev/build-mac.sh
+++ plugin-dev/build-mac.sh
@@ -2,26 +2,27 @@
 set -x
 set -e
 
-rm -rf build-mac
+#rm -rf build-mac
 rm -rf dist/mac
 
 for arch in arm64 x86_64
 do
-cmake -GNinja \
+#cmake -GNinja \
+cmake -GXcode \
     -B build-mac/$arch \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 \
     -DCMAKE_INSTALL_PREFIX=build-mac/install/$arch \
-    -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_OSX_ARCHITECTURES=$arch
-ninja -C build-mac/$arch install
+#ninja -C build-mac/$arch install
+cmake --build build-mac/$arch --target install --config Release
 done
 
 mkdir -p dist/mac/include dist/mac/lib
-cp -rv build-mac/install/x86_64/include/* dist/mac/include
+#cp -rv build-mac/install/x86_64/include/* dist/mac/include
 xcrun lipo -create \
-    build-mac/install/arm64/lib/libAES.a \
-    build-mac/install/x86_64/lib/libAES.a \
+    build-mac/install/arm64/lib/libhello_cocos.a \
+    build-mac/install/x86_64/lib/libhello_cocos.a \
     -output \
-    dist/mac/lib/libAES.a 
+    dist/mac/lib/libhello_cocos.a 
 
 find dist/mac -name "*.a" -exec xcrun strip -S {} \;
